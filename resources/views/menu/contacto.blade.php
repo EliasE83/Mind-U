c376423f6d5ef0933b4469b3f428932a8ed81213
EdgarEscobedoZ <125921549+EdgarEscobedoZ@users.noreply.github.com>
2024-06-04 22:49:08 -0600

    Avance de vista  "contacto"
    Se creo la sección con nuestra información de contacto y para enviar nuevas preguntas directamente a los desarrolladores

--- resources/views/menu/contacto.blade.php
+++ resources/views/menu/contacto.blade.php
@@ -1,11 +1,10 @@
-@extends('layouts.template')
-
 
 @push('head')
 @vite('resources/css/contacto.css')
 @vite('resources/js/contacto.js')
 @endpush
 
+@extends('layouts.template')
 
 @section('content')
 
@@ -18,52 +17,83 @@
 {{-- Fin  Div del menú --}}
 
 
-    <div class="flex flex-auto items-center justify-center">
+<div class="flex flex-auto items-center justify-center">
 
-        <div class="container">
-        <h1 class="text-4xl font-bold text-center mb-5 ">¿Cómo podemos ayudarte?</h1>
-            <h2>Frequently Asked Questions</h2>
-            <div class="accordion">
-                <div class="accordion-item">
-                    <button id="accordion-button-1" aria-expanded="false"><span class="accordion-title">Why is the moon sometimes out during the day?</span><span class="icon" aria-hidden="true"></span></button>
-                        <div class="accordion-content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
-                        </div>
-                </div>
+<div class="container">
 
-                <div class="accordion-item">
-                    <button id="accordion-button-2" aria-expanded="false"><span class="accordion-title">Why is the sky blue?</span><span class="icon" aria-hidden="true"></span></button>
-                        <div class="accordion-content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
-                        </div>
+    <h2 >FAQ</h2>
+
+    <div class="accordion">
+
+        <div class="accordion-item ">
+            <button id="accordion-button-1" aria-expanded="false"><span class="accordion-title">Why is the moon sometimes out during the day?</span><span class="icon" aria-hidden="true"></span></button>
+                <div class="accordion-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
                 </div>
-                <div class="accordion-item">
-                    <button id="accordion-button-3" aria-expanded="false"><span class="accordion-title">Will we ever discover aliens?</span><span class="icon" aria-hidden="true"></span></button>
-                        <div class="accordion-content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
-                        </div>
-                </div>
-                <div class="accordion-item">
-                    <button id="accordion-button-4" aria-expanded="false"><span class="accordion-title">How much does the Earth weigh?</span><span class="icon" aria-hidden="true"></span></button>
-                        <div class="accordion-content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
-                        </div>
-                </div>
-                <div class="accordion-item">
-                    <button id="accordion-button-5" aria-expanded="false"><span class="accordion-title">How do airplanes stay up?</span><span class="icon" aria-hidden="true"></span></button>
-                        <div class="accordion-content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
-                        </div>
-                </div>
-            </div>
         </div>
 
+        <div class="accordion-item">
+            <button id="accordion-button-2" aria-expanded="false"><span class="accordion-title">Why is the sky blue?</span><span class="icon" aria-hidden="true"></span></button>
+                <div class="accordion-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                </div>
+        </div>
+        <div class="accordion-item ">
+            <button id="accordion-button-3" aria-expanded="false"><span class="accordion-title">Will we ever discover aliens?</span><span class="icon" aria-hidden="true"></span></button>
+                <div class="accordion-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                </div>
+        </div>
+        <div class="accordion-item ">
+            <button id="accordion-button-4" aria-expanded="false"><span class="accordion-title">How much does the Earth weigh?</span><span class="icon" aria-hidden="true"></span></button>
+                <div class="accordion-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                </div>
+        </div>
+        <div class="accordion-item ">
+            <button id="accordion-button-5" aria-expanded="false"><span class="accordion-title">How do airplanes stay up?</span><span class="icon" aria-hidden="true"></span></button>
+                <div class="accordion-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                </div>
+        </div>
+        
+
     </div>
-   
+    
+    <div class='container-contact'>
+        <div class="card">
+            <h3>Contáctanos</h3>
+            <label>Correo electronico: correo@mindu.com</label>
+            <label></label>
+        </div>
+    </div>
+
+    
+
 </div>
+
+<div>
+    <div class="container-forms">
+        <form>
+            <label for="message">Nueva Pregunta</label>
+            <textarea id="message" cols="30" rows="10" placeholder="Message"></textarea>
+            <input type="submit" value="Enviar">
+        </form>
+    </div>
+
+
+    </div>  
+
+
+
+</div>
+
+</div>
+
+
 {{-- Fin de Div principal --}}
 
-{{-- Footer --}}
+{{-- Footer --}}    
 {{-- Fin Footer --}}
 
 @endsection
